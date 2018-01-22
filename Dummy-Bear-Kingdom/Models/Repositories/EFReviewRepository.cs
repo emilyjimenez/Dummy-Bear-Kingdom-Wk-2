b6ea55470839d41470fffccc27f56720ce72c2ac
Emily Wells Jimenez <emwellspdx@gmail.com>
2018-01-22 08:04:48 -0800

    add I/EF repository for reviews and build view models.

---
+++ Dummy-Bear-Kingdom/Models/Repositories/EFReviewRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DummyBearKingdom.Models
+{
+    public class EFReviewRepository : IReviewRepository
+    {
+        DummyBearKingdomDbContext db = new DummyBearKingdomDbContext();
+
+        public IQueryable<Review> Reviews
+        { get { return db.Reviews; }}
+
+        public Review Save(Review review)
+        {
+            db.Products.Add(review);
+            db.SaveChanges();
+            return review;
+        }
+
+        public Review Edit(Review review)
+        {
+            db.Entry(review).State = EntityState.Modified;
+            db.SaveChanges();
+            return review;
+        }
+
+        public void Remove(Review review)
+        {
+            db.Products.Remove(review);
+            db.SaveChanges();
+        }
+    }
+}
